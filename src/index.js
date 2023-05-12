09a2506425ed6cf8f2c43f388846e2320ce78a42
Brenden Olding <oldingb21@gmail.com>
2023-05-12 15:06:36 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+const fetchMovie = (input) => {
+    fetch(`http://localhost:3000/movies/${input}`)
+        .then((res) => res.json())
+        .then((movie) => {
+            const title = document.querySelector("#movieDetails h4")
+            const summary = document.querySelector("#movieDetails p")
+            title.textContent = movie.title;
+            summary.textContent = movie.summary;
+        })
+}
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form')  
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        fetchMovie(e.target.children[1].value)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
